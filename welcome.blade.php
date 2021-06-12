413850118c1476d4937e12b79d3a983b28632d37
Ruhin012 <85052397+Ruhin012@users.noreply.github.com>
2021-06-12 12:27:03 +0600

    Add files via upload
    Here I have added my first week update for project-200 task. It includes the store management site welcome page view.

---
+++ welcome.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>&#128110; Ruhin Munshi</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: lightblue;
+                color: black;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 54px;
+            }
+
+            .links > a {
+                color: #0000EE;
+                padding: 0 25px;
+                font-size: 20px;
+                font-weight: 1200;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+                    <div class="content">
+                <div class="title m-b-md">
+                   
+                    <hr/>
+                    <p>CHILLHUT CLOTHING <br/>
+                       Find Your Beauty Here<br/>
+                       
+                   </p>
+                </div>
+                
+            </div>
+       
+    </body>
+</html>
